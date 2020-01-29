0e218c1002932684985b654323183656b56cf98b
John E. Kwan <John.Kwan@novasprint.com>
2020-01-29 17:58:24 +0800

    Added email template changes

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,46 +19,47 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
 
-        var data = {
-          service_id: 'smtp_server',
-          template_id: 'template_n5uxH0J1',
-          user_id: 'user_S64QlZRZsvoEhAd4tjG7I',
-          template_params: {
-              'to_name': 'John',
-              'from_name': name,
-              'message_html': message
-          }
+      var data = {
+        service_id: 'smtp_server',
+        template_id: 'template_n5uxH0J1',
+        user_id: 'user_S64QlZRZsvoEhAd4tjG7I',
+        template_params: {
+          'reply_to': email,
+          'to_name': 'John Eric',
+          'from_name': name,
+          'message_html': message
+        }
       };  
 
       $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-    type: 'POST',
-    data: JSON.stringify(data),
-    contentType: 'application/json'
-}).done(function() {
-    //Success message
-          $('#success').html("<div class='alert alert-success'>");
-          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
-          $('#success > .alert-success')
-            .append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-}).fail(function(error) {
-    //alert('Oops... ' + JSON.stringify(error));
-          $('#success').html("<div class='alert alert-danger'>");
-          $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-            .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
-          $('#success > .alert-danger').append('</div>');
-          //clear all fields
-          $('#contactForm').trigger("reset");
-}).always(function() {
-  setTimeout(function() {
-  $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
-  }, 1000);
-});;
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+      }).done(function () {
+        //Success message
+        $('#success').html("<div class='alert alert-success'>");
+        $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+        $('#success > .alert-success')
+          .append("<strong>Your message has been sent. </strong>");
+        $('#success > .alert-success')
+          .append('</div>');
+        //clear all fields
+        $('#contactForm').trigger("reset");
+      }).fail(function (error) {
+        //alert('Oops... ' + JSON.stringify(error));
+        $('#success').html("<div class='alert alert-danger'>");
+        $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+          .append("</button>");
+        $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+        $('#success > .alert-danger').append('</div>');
+        //clear all fields
+        $('#contactForm').trigger("reset");
+      }).always(function () {
+        setTimeout(function () {
+          $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+        }, 1000);
+      });;
 
       // $.ajax({
       //   url: "././mail/contact_me.php",
